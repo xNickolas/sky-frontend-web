817753f0755000052ebb572729b73edd1272b748
xNickolas <nickolsctt@gmail.com>
2020-11-13 00:37:08 -0300

    general changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,52 +108,19 @@ fetch('https://sky-frontend.herokuapp.com/movies')
     })
 
 
-
-
-
-
-
-
-
-
-
-
 // ------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // const app = document.getElementById('page-content');
 
-// const container = document.createElement('div');
-// container.setAttribute('class', 'container');
-
-// app.appendChild(container);
-
-
-// Create a request variable and assign a new XMLHttpRequest object to it.
 // var request = new XMLHttpRequest();
 
-// // // Open a new connection, using the GET request on the URL endpoint
-// request.open('GET', 'https://sky-frontend.herokuapp.com/movies', true);
+// request.open('GET', 'https://sky-frontend.herokuapp.com/movies');
 
 // request.onload = function () {
-//  // Begin accessing JSON data here
 //   var data = JSON.parse(this.response);
-
   
 //   data.forEach(movies => {
-
 //     let element = document.getElementById('page-content')
 //     element.innerHTML = `<p>${movies.movies}</p>`
 
@@ -164,16 +131,8 @@ fetch('https://sky-frontend.herokuapp.com/movies')
 //   // const card = document.createElement('div');
 //   // card.setAttribute('class', 'card');
 
-
 //   // const h3 = document.createElement('h3');
 //   // h3.textContent = movies.title;
-
-//   // const img = document.createElement('img');
-//   // img.src = movies.movies.images;
-  
-//   // container.appendChild(card);
-//   // card.appendChild(h3);
-//   // card.appendChild(img)
 
 // }
 
